1b4870fb04422fe2b5827498ede7abb99c664497
Eddy <miguepoke28@gmail.com>
2020-02-15 10:42:20 -0500

    Authentication screen added

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,7 @@ import './providers/orders.dart';
 import './screens/orders_screen.dart';
 import './screens/user_products_screen.dart';
 import './screens/edit_product_screen.dart';
+import './screens/auth_screen.dart';
 
 void main() => runApp(MyApp());
 
@@ -37,7 +38,7 @@ class MyApp extends StatelessWidget {
           fontFamily: 'Lato',
         ),
         debugShowCheckedModeBanner: false,
-        home: ProductsOverviewScreeen(),
+        home: AuthScreen(),
         routes: {
           ProductDetailScreen.routeName: (context) => ProductDetailScreen(),
           CartScreen.routeName: (context) => CartScreen(),
